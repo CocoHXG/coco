35a74acce8260e2aa58f69c99cec2796bf050e59
Ray Kwan <kwanlokto@gmail.com>
2019-11-02 08:34:53 -0400

    create inputs

--- app/src/App.js
+++ app/src/App.js
@@ -1,53 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import request from 'request';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link,
 } from "react-router-dom";
 import './App.css';
 import {FraudDetection} from './fraudDetection'
 
 function App() {
   var userIDs = undefined;
+  const [inputs, setInputs] = useState({});
 
-  var getUsers = {
-    method: 'POST',
-    url: 'https://api.td-davinci.com/api/raw-customer-data',
-    headers: {
-      'Authorization': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJDQlAiLCJ0ZWFtX2lkIjoiZjZhZWQyMTEtZTk5YS0zNjE2LThmZTAtYmYxZmE0YmRiNWMzIiwiZXhwIjo5MjIzMzcyMDM2ODU0Nzc1LCJhcHBfaWQiOiJmNGFhMjZlYS04Yzk3LTQzOGYtOWQyYy1lODMzZTM4NDFhNTYifQ.MpK7XJ_B482aLh6J04-iafgindgH21GZ2rEtsdSsuLA'
-    }
-  };
-  
-  function accountCallback(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      const info = JSON.parse(body);
-      console.log(info.result)
+  const handleInputChange = (event) => {
+    event.persist();
+    setInputs(inputs => ({...inputs, [event.target.name]: event.target.value}));
+  }
+
+  const handleSubmit = (event) => {
+    if (event) {
+      event.preventDefault();
+      alert(`User Created!
+         Name: ${inputs.userID} ${inputs.purchase}`);
     }
   }
 
-  function callback(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      const info = JSON.parse(body);
-      var customers = info.result.customers;
-      customers.forEach(customer => {
-        var getAccount = {
-          method: 'GET',
-          url: `https://api.td-davinci.com/api/customers/${customer.id}/accounts`,
-          headers: {
-            'Authorization': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJDQlAiLCJ0ZWFtX2lkIjoiZjZhZWQyMTEtZTk5YS0zNjE2LThmZTAtYmYxZmE0YmRiNWMzIiwiZXhwIjo5MjIzMzcyMDM2ODU0Nzc1LCJhcHBfaWQiOiJmNGFhMjZlYS04Yzk3LTQzOGYtOWQyYy1lODMzZTM4NDFhNTYifQ.MpK7XJ_B482aLh6J04-iafgindgH21GZ2rEtsdSsuLA'
-          }
-        };
-        request(getAccount, accountCallback);
-      });
-    }
-  }
-  
-  request(getUsers, callback);
-  
   return (
-    <h1> GOOD </h1>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>User ID</label>
+        <input type="text" name="userID" value={inputs.userID} onChange={handleInputChange} required />
+        <label>Purchase</label>
+        <input type="number" name="purchase" value={inputs.purchase} onChange={handleInputChange} required />
+      </div>
+      <button type="submit" >Verify</button>
+    </form>
   );
 }
 
