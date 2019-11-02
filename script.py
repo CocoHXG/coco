bc0fb6db975b5992463e30a1d49604bce06d28e0
Ray Kwan <kwanlokto@gmail.com>
2019-11-02 16:43:41 -0400

    get endpoint working

--- script.py
+++ script.py
@@ -1,10 +1,7 @@
 # install the requests package using 'python -m pip install requests'
 import json
 from collections import namedtuple
-
 import requests
-
-#import rhinoscriptsyntax as rs
 
 apiKey = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJDQlAiLCJ0ZWFtX2lkIjoiZjZhZWQyMTEtZTk5YS0zNjE2LThmZTAtYmYxZmE0YmRiNWMzIiwiZXhwIjo5MjIzMzcyMDM2ODU0Nzc1LCJhcHBfaWQiOiJmNGFhMjZlYS04Yzk3LTQzOGYtOWQyYy1lODMzZTM4NDFhNTYifQ.MpK7XJ_B482aLh6J04-iafgindgH21GZ2rEtsdSsuLA'
 start = 'https://api.td-davinci.com/api/'
@@ -24,21 +21,16 @@ for customer in customers:
         headers = { 'Authorization': apiKey},
         json = { 'continuationToken': '' }
     ).json()['result']
-    transaction_info = []
 
     for transaction in transactions:
         if "locationLatitude" in transaction:
-            transaction_info.append([
+            customer_info.append([
+                customer["id"],
+                customer["addresses"]["principalResidence"]["latitude"],
+                customer["addresses"]["principalResidence"]["longitude"],
                 transaction["currencyAmount"], 
                 transaction["locationLatitude"], 
                 transaction["locationLongitude"]])
-    
-    customer_info.append([
-        customer["id"],
-        customer["addresses"]["principalResidence"]["latitude"],
-        customer["addresses"]["principalResidence"]["longitude"],
-        transaction_info
-        ])
 
-with open('customer_info_row.json', 'w') as outfile:
+with open('customer_info_row.py', 'w') as outfile:
     json.dump(customer_info, outfile)
--- src/app.py
+++ src/app.py
@@ -1,17 +1,47 @@
 import falcon
+import pandas as pd
 from sklearn import tree
+from customer_info_row import CUSTOMERS
+
 
 class DecisionTree(object):
     def on_get(self, req, resp):
-        customer_id = req.media
-        print(customer_id)
+        print(req.query_string)
+        try:
+            customer_id = req.params["customer_id"]
+            X_test = [[req.params["cost"], req.params["lat"], req.params["long"]]]
+        except KeyError:
+            raise falcon.HTTPBadRequest()
+        
+        rank = 0
+        X = []
+        Y = []
+        prev_customer = None
+        customer_rank = -1
+        for row in CUSTOMERS:
+            curr_customer = row[0]
+            if curr_customer != prev_customer:
+                customer_rank += 1
+                X.append([0] + row[1: 3])
+                Y.append(customer_rank)
+            if curr_customer == customer_id:
+                rank = customer_rank
+            X.append(row[3: ])
+            Y.append(customer_rank)
+            prev_customer = row[0]
+        
+        
+        
+        clf = tree.DecisionTreeClassifier()
+        clf = clf.fit(X, Y)
+        y_pred = clf.predict(X_test)
+
         resp.status = falcon.HTTP_200
-        resp.body = customer_id
-        # X = [[0, 0], [1, 1]]
-        # Y = [0, 1]
-        # clf = tree.DecisionTreeClassifier()
-        # clf = clf.fit(X, Y)
-        # print("ENTERED")
+        print(y_pred)
+
+        if Y[0] != y_pred:
+            resp.status = falcon.HTTP_203
+        print("SUCCESS")
 
 app = application = falcon.API()
 things = DecisionTree()
